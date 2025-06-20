87168085cecb12e4eac9e7a7a442948570594da8
lemonlambda <lemonjameslem@protonmail.com>
2025-06-20 15:21:34 -0400

    Added organic processed data card

---
+++ prototypes/organic-processed-data.lua
@@ -0,0 +1,10 @@
+local organic_processed_data = {
+  type = "tool",
+  name = "organic-processed-data",
+  icon = "__Krastorio2Assets__/icons/cards/matter-research-data.png",
+  stack_size = 100,
+}
+
+data:extend{
+  organic_processed_data
+}
